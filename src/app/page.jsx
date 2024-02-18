4580008cd6813ef438b50224bead9bdc81d8170d
Devarshi <devarshi224@gmail.com>
2024-02-18 23:21:56 +0530

    trial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,15 +9,15 @@ export default function Home() {
     <div className={Styles.container}>
       <div className={Styles.textContainer}>
         <div>
-      <div className={Styles.fname}>Devarshi</div>
-      <div className={Styles.name}>Sanghani</div>
-      </div>
-      <div className={Styles.desc}>Hey, I'm the software engineer you want in your corner.
-      I don't just write lines, 
-       I sculpt solutions that stand out. I'm all about the details and delivering nothing but the best.
-        Let's team up and take your digital dreams to the next level. 
-      Trust me, we'll make some serious waves in the realm of software.</div>
-      <div className={`${Styles.myFont} ${myFont.className}`}>Mind Over Matter</div>
+          <div className={Styles.fname}>Devarshi</div>
+          <div className={Styles.name}>Sanghani</div>
+        </div>
+        <div className={Styles.desc}>{`Hey, I'm the software engineer you want in your corner.
+          I don't just write lines,
+          I sculpt solutions that stand out. I'm all about the details and delivering nothing but the best.
+          Let's team up and take your digital dreams to the next level.
+          Trust me, we'll make some serious waves in the realm of software.`}</div>
+        <div className={`${Styles.myFont} ${myFont.className}`}>Mind Over Matter</div>
       </div>
       <div className={Styles.imgContainer}>
         <Image
