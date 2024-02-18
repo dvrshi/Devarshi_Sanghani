c64d5a148d61015ed48f8a80bc5bcdcb7adda098
Devarshi <devarshi224@gmail.com>
2024-02-18 23:13:01 +0530

    hosting

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
+import localFont from "next/font/local";
 import Styles from "./Home.module.css";
+const myFont = localFont({
+  src: '../../public/Creattion Demo.otf',
+});
 export default function Home() {
   return (
     <div className={Styles.container}>
@@ -8,8 +12,12 @@ export default function Home() {
       <div className={Styles.fname}>Devarshi</div>
       <div className={Styles.name}>Sanghani</div>
       </div>
-      <div className={Styles.desc}></div>
-      <div className={Styles.desc} ></div>
+      <div className={Styles.desc}>Hey, I'm the software engineer you want in your corner.
+      I don't just write lines, 
+       I sculpt solutions that stand out. I'm all about the details and delivering nothing but the best.
+        Let's team up and take your digital dreams to the next level. 
+      Trust me, we'll make some serious waves in the realm of software.</div>
+      <div className={`${Styles.myFont} ${myFont.className}`}>Mind Over Matter</div>
       </div>
       <div className={Styles.imgContainer}>
         <Image
